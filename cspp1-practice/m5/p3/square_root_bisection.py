4033a590e0c99777fde22251ca6f90f8cab74934
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-04 17:29:28 +0530

    " -> 3 of 7 passed. pylint: 10.00/10 "

--- cspp1-practice/m5/p3/square_root_bisection.py
+++ cspp1-practice/m5/p3/square_root_bisection.py
@@ -1,13 +1,28 @@
-"""Bisection"""
-A = int(input())
-E = 0.01
-L = 0.0
-H = A
-ANS = (H + L)/2.0
-while abs(ANS**2 - A) >=E:
-    if ANS**2 < A:
-        L = ANS
-    else:
-        H = ANS
-    ANS = (H + L)/2.0
-print(ANS)
+'''# Write a python program to find the square root of the given number
+# using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+'''
+def main():
+    ''' this program is used to print the squareroot value of
+    the given number using bi-section method'''
+    num_val = int(input())
+    epsilon = 0.01
+    low = 0
+    high = num_val
+    avg = (low+high)/2
+    while abs(avg**2-num_val) >= epsilon:
+        if avg**2 < num_val:
+            low = avg
+        else:
+            high = avg
+        avg = (low+high)/2
+    print(avg)
+if __name__ == "__main__":
+    main()
