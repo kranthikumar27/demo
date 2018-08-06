dde932d6a4f98db79bd582359b2f8c9c86d95cf0
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-06 22:49:27 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- cspp1-practice/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-practice/m7/Functions - Assignment-1/assignment1.py
@@ -39,7 +39,7 @@ def main():
 	data = input()
 	data = data.split(' ')
 	data = list(map(float, data))
-	print("Remaining balance:"paying_Debt_Off_In_A_Year(data[0],data[1],data[2]))
+	print("Remaining balance:"paying_debt_off_in_a_year(data[0],data[1],data[2]))
 
 if __name__== "__main__":
 	main()
