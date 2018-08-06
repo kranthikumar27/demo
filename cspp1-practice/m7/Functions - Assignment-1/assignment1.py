e40cdd652e989c0239184d8d70dda8a2edd4fa3f
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-07 00:23:45 +0530

    " -> 10 of 10 passed. pylint: 4.38/10 "

--- cspp1-practice/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-practice/m7/Functions - Assignment-1/assignment1.py
@@ -1,4 +1,4 @@
-# Functions | Assignment-1 - Paying Debt off in a Year
+'''# Functions | Assignment-1 - Paying Debt off in a Year
 
 # Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the
 # credit card company each month.
@@ -23,25 +23,25 @@
 # Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
 # Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
 # Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
+'''
 
 def paying_debt_off_in_a_year(balance, annual_interest_rate, monthly_payment_rate):
-        previous_balance = balance
-        for _ in range(12):
-            monthly_interest_rate = (annual_interest_rate / 12.0)
-            minimum_monthly_payment = (monthly_payment_rate) * (previous_balance)
-            monthly_unpaid_balance = (previous_balance) - (minimum_monthly_payment)
-            updated_balance_each_month = (monthly_unpaid_balance) + (monthly_interest_rate * monthly_unpaid_balance)
-            previous_balance = updated_balance_each_month
-        return round(updated_balance_each_month,2)
+    previous_balance = balance
+    for _ in range(12):
+        monthly_interest_rate = (annual_interest_rate / 12.0)
+        minimum_monthly_payment = (monthly_payment_rate) * (previous_balance)
+        monthly_unpaid_balance = (previous_balance) - (minimum_monthly_payment)
+        updated_balance_each_month = (monthly_unpaid_balance) + (monthly_interest_rate * monthly_unpaid_balance)
+        previous_balance = updated_balance_each_month
+    return round(updated_balance_each_month, 2)
                 
 def main():
     '''this program is used to print the remaining balance of credit cvard after one year '''
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Remaining balance:", paying_debt_off_in_a_year (data[0],data[1],data[2]))
+    print("Remaining balance:", paying_debt_off_in_a_year (data[0], data[1], data[2]))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
 
