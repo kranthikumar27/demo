d81810af76581d17bee18f1e158e743ac9621d29
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-07 20:00:21 +0530

    " -> 9 of 10 passed. pylint: -1.11/10 "

--- cspp1-practice/m8/p1/assignment1.py
+++ cspp1-practice/m8/p1/assignment1.py
@@ -12,9 +12,9 @@ def factorial(N):
     '''
     # Your code here
     if N == 1 or N == 1:
-    	return 1
+        return 1
     else:
-    	return N * factorial( N - 1)
+        return N * factorial(N - 1)
     
 
 
