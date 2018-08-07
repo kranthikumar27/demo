48e55630a69398c6d18560fe6e7f23ce92b498d1
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-07 21:54:58 +0530

    gcd

--- cspp1-practice/m8/GCD using Iteration/gcd_iter.py
+++ cspp1-practice/m8/GCD using Iteration/gcd_iter.py
@@ -11,13 +11,19 @@ def gcdIter(a, b):
     returns: a positive integer, the greatest common divisor of a & b.
     '''
     # Your code here
+    c = min(a,b)
+    while c > 0:
+        if(a%c) == 0 and (b%c) == 0:
+            return c
+        else:
+            c -= 1
     
 
 
 
 def main():
     data = input()
-    data = data.split()
+    data = data.split(" ")
     print(gcdIter(int(data[0]),int(data[1]))) 
 
 if __name__== "__main__":
