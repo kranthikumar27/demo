fffccd1986fdb4eab296e53331fa43c1bd25928c
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-07 20:33:47 +0530

    " -> 10 of 10 passed. pylint: 8.89/10 "

--- cspp1-practice/m8/p1/assignment1.py
+++ cspp1-practice/m8/p1/assignment1.py
@@ -9,10 +9,13 @@ def factorial(num_val):
     returns: a positive integer, the factorial of n.
     '''
     # Your code here
-    if num_val in (1, 0)
+    if num_val in [0, 1]:
         return 1
     return num_val * factorial(num_val - 1)
 def main():
+    '''
+    fact
+    '''
     a = input()
     print(factorial(int(a)))
 
