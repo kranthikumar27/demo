a06eb8410bbc65cc765353ba724992f2374dd248
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-25 08:19:05 +0530

    " -> 14 of 14 passed. pylint: 7.14/10 "

--- cspp1-practice/m21/tic_tac_toe.py
+++ cspp1-practice/m21/tic_tac_toe.py
@@ -16,22 +16,22 @@ def game_winner(game):
 
     if abs(counter['x'] - counter['o']) == 1:
         if ((game[0][0] == 'x' and game[0][1] == 'x' and game[0][2] == 'x') or 
-            (game[1][0] == 'x' and game[1][1] == 'x' and game[1][2] == 'x') or
-            (game[2][0] == 'x' and game[2][1] == 'x' and game[2][2] == 'x') or
-            (game[0][0] == 'x' and game[1][0] == 'x' and game[2][0] == 'x') or
-            (game[0][1] == 'x' and game[1][1] == 'x' and game[2][1] == 'x') or
-            (game[0][2] == 'x' and game[1][2] == 'x' and game[2][2] == 'x') or
-            (game[0][0] == 'x' and game[1][1] == 'x' and game[2][2] == 'x') or
-            (game[2][0] == 'x' and game[1][1] == 'x' and game[0][2] == 'x')):
+                (game[1][0] == 'x' and game[1][1] == 'x' and game[1][2] == 'x') or
+                (game[2][0] == 'x' and game[2][1] == 'x' and game[2][2] == 'x') or
+                (game[0][0] == 'x' and game[1][0] == 'x' and game[2][0] == 'x') or
+                (game[0][1] == 'x' and game[1][1] == 'x' and game[2][1] == 'x') or
+                (game[0][2] == 'x' and game[1][2] == 'x' and game[2][2] == 'x') or
+                (game[0][0] == 'x' and game[1][1] == 'x' and game[2][2] == 'x') or
+                (game[2][0] == 'x' and game[1][1] == 'x' and game[0][2] == 'x')):
             return 'x'
         if ((game[0][0] == 'o' and game[0][1] == 'o' and game[0][2] == 'o') or 
-            (game[1][0] == 'o' and game[1][1] == 'o' and game[1][2] == 'o') or
-            (game[2][0] == 'o' and game[2][1] == 'o' and game[2][2] == 'o') or
-            (game[0][0] == 'o' and game[1][0] == 'o' and game[2][0] == 'o') or
-            (game[0][1] == 'o' and game[1][1] == 'o' and game[2][1] == 'o') or
-            (game[0][2] == 'o' and game[1][2] == 'o' and game[2][2] == 'o') or
-            (game[0][0] == 'o' and game[1][1] == 'o' and game[2][2] == 'o') or
-            (game[2][0] == 'o' and game[1][1] == 'o' and game[0][2] == 'o')):
+                (game[1][0] == 'o' and game[1][1] == 'o' and game[1][2] == 'o') or
+                (game[2][0] == 'o' and game[2][1] == 'o' and game[2][2] == 'o') or
+                (game[0][0] == 'o' and game[1][0] == 'o' and game[2][0] == 'o') or
+                (game[0][1] == 'o' and game[1][1] == 'o' and game[2][1] == 'o') or
+                (game[0][2] == 'o' and game[1][2] == 'o' and game[2][2] == 'o') or
+                (game[0][0] == 'o' and game[1][1] == 'o' and game[2][2] == 'o') or
+                (game[2][0] == 'o' and game[1][1] == 'o' and game[0][2] == 'o')):
             return 'o'
         return 'draw'
 
