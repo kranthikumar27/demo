1d07305f64c3cc716ad13f054afd259c3ce2370a
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-12-20 11:11:19 +0530

    " -> 0 of 3 passed. pylint: 6.77/10 "

--- cspp1-practice/m4/p2/bob_counter.py
+++ cspp1-practice/m4/p2/bob_counter.py
@@ -1,12 +1,10 @@
-s = input()
-count = 0
-i = 0
-j = 1
-k = 2
-while j < (len(s)):
-    if s[i] == 'b' and s[j] == 'o' and s[k] == 'b':
-        count=count+1
-    i = i + 1
-    j = j + 1
-    k = k + 1
-print(count)
+def main():
+    ''' this program is used to print the bob count in a given string'''
+    str_val = input()
+    count_val = 0
+    for char_val in range(len(str_val) - 2):
+        if str_val[char_val] == 'b' and str_val[char_val+1] == 'o' and str_val[char_val+2] == 'b':
+            count_val += 1
+    print(count_val)
+if __name__ == "__main__":
+    main()
--- cspp1-practice/m6/p1/fizz_buzz.py
+++ cspp1-practice/m6/p1/fizz_buzz.py
@@ -4,18 +4,22 @@ For each multiple of 3, print "Fizz" instead of the number.
 For each multiple of 5, print "Buzz" instead of the number.
 For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 '''
-n=int(input())
-
-
-for i in range(1,n+1,1):
-    if i%3==0 and i%5==0:
-        print("Fizz")
-        print("Buzz")
-       
-    elif i%3==0:
-        print("Fizz")
-     
-    elif i%5==0:
-        print("Buzz")
-    else:
-        print(i)
+def main():
+    '''
+    Read number from the input, store it in variable num.
+    '''
+    num_val = int(input())
+    if num_val <= 0:
+        print()
+    for num_vala in range(1, num_val+1):
+        if(num_vala%3 == 0 and num_vala%5 == 0):
+            print("Fizz")
+            print("Buzz")
+        elif num_vala%3 == 0:
+            print("Fizz")
+        elif num_vala%5 == 0:
+            print("Buzz")
+        else:
+            print(num_vala)
+if __name__ == "__main__":
+    main()
