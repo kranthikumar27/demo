1f157a26ba0533d135c4e8949a9dcc3f56b8ee61
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-12-21 11:48:00 +0530

    "cspp1_m10_a1_1: 4/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 6/10 "

---
+++ cspp1-practice/CSPP-1_Question/sudoku.py
@@ -0,0 +1,84 @@
+'''
+    Sudoku is a logic-based, combinatorial number-placement puzzle.
+    The objective is to fill a 9×9 grid with digits so that
+    each column, each row, and each of the nine 3×3 subgrids that compose the grid
+    contains all of the digits from 1 to 9.
+    Complete the check_sudoku function to check if the given grid
+    satisfies all the sudoku rules given in the statement above.
+'''
+def check_sudoku(elements):
+    '''
+        Your solution goes here. You may add other helper functions as needed.
+        The function has to return True for a valid sudoku grid and false otherwise
+    '''
+    if len(elements) < 81 or len(elements) > 81:
+        return "Invalid input"
+    n = 9
+    [elements[i:i+n] for i in range(0, len(elements), n)]
+    try:
+        for i in range(len(elements)):
+            temp = i.copy()
+            list_1 = []
+            int(temp.sort)
+            if ''.join(temp) != '123456789':
+                return "Invalid Sudoku:Duplicate values"
+            for j in range(len(elements)):
+                list_1.append(elements[j][i])
+            list_1.sort()
+            if ''.join(list_1) != '123456789':
+                return "Invalid Sudoku:Duplicate values"
+    except:
+            return "Invalid Sudoku:Duplicate values"
+
+            
+
+    
+
+
+
+    # splitting : [line[i:i+n] for i in range(0, len(line), n)]
+    # row = []
+    # for each_row in sudoku:
+    #     row = each_row.copy()
+    #     row.sort()
+    #     if ''.join(row) != '123456789':
+    #         return False
+    # # print(sudoku)
+    # for i in range(9):
+    #     list_1 = []
+    #     for j in range(9):
+    #         # print(j,i)
+    #         # print(sudoku[j][i])
+    #         list_1.append(sudoku[j][i])
+    #     # print(list_1)
+    #     list_1.sort()
+    #     if ''.join(list_1) != '123456789':
+    #         return False
+    # for k in range(0, 7, 3):
+    #     # count_ofloops = 0
+    #     # while count_ofloops < 3:
+    #     for i in range(k, k+3):
+    #         list_2 = []
+    #         for col_ran in range(0, 7, 3):
+    #             for j in range(col_ran, col_ran+3):
+    #                 # print(sudoku[j][i])
+    #                 list_2.append(sudoku[j][i])
+    #         # print(list_2)
+    #         list_2.sort()
+    #         # print(list_2)
+    #         if ''.join(list_2) != '123456789':
+    #             return False
+    #         # count_ofloops += 1
+    # return True
+
+
+def main():
+    '''
+        main function to read input sudoku from console
+        call check_sudoku function and print the result to console
+    '''
+    elements = str(input())
+    print(check_sudoku(elements))
+
+if __name__ == '__main__':
+    main()
