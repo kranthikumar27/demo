824b02574a7df3402dea81f6b1145a18823c29bb
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-12-29 11:23:00 +0530

    "cspp1_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 4/10 "

--- cspp1-practice/remideals/CSPP-1Week1Exam/webpage.py
+++ cspp1-practice/remideals/CSPP-1Week1Exam/webpage.py
@@ -18,9 +18,9 @@ def getBackground(color):
         pass
 
 def getImageUrl(urls):
-    lines = urls.split("<img src")
+    lines = urls.split("<img")
     # print(lines)
-    startingtag="=\""
+    startingtag=" \""
     endtag = "\""
     image=[]
     counter=0
