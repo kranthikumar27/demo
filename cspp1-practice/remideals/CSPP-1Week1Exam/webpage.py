5e639acc4f6cdf7831cf0fdf7c908582082eb3dc
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-12-29 14:35:43 +0530

    "cspp1_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 2/10 "

--- cspp1-practice/remideals/CSPP-1Week1Exam/webpage.py
+++ cspp1-practice/remideals/CSPP-1Week1Exam/webpage.py
@@ -1,40 +1,41 @@
-def background(color):
-    lines=color.split("back")
-    initialtag="ground-color:\""
-    endtagg=";\""
-    bgcolor=[]
-    count=0
-    for i in lines:
-        bgcolor.append(i)
-        for j in bgcolor:
-            if initialtag in j:
-                start = j.index(initialtag)
-                items = j[start+len(initialtag):]
-                end = j.index(endtagg)
-                answer = j[:end]
-        count=count+1
-        print(answer)
-    print(count)
-
-# def image(urls):
-#     lines = urls.split("<img src")
-#     # print(lines)
-#     startingtag="=\""
-#     endtag = "\""
-#     image=[]
-#     counter=0
+# def background(color):
+#     lines=color.split("back")
+#     initialtag="ground-color:\""
+#     endtagg=";\""
+#     bgcolor=[]
+#     count=0
+#     answer=0
 #     for i in lines:
-#         image.append(i)
-#         # print(image)
-#         for items in image:
-#             if startingtag in items:
-#                 start = items.index(startingtag)
-#                 items = items[start+len(startingtag):]
-#                 end = items.index(endtag)
-#                 result = items[:end]
-#         counter=counter+1
-#         print(result)
-#     print(counter)
+#         bgcolor.append(i)
+#         for j in bgcolor:
+#             if initialtag in j:
+#                 start = j.index(initialtag)
+#                 items = j[start+len(initialtag):]
+#                 end = j.index(endtagg)
+#                 answer = j[:end]
+#         count=count+1
+#         print(answer)
+#     print(count)
+
+def image(urls):
+    lines = urls.split("<img src")
+    # print(lines)
+    startingtag="=\""
+    endtag = "\""
+    image=[]
+    counter=0
+    for i in lines:
+        image.append(i)
+        # print(image)
+        for items in image:
+            if startingtag in items:
+                start = items.index(startingtag)
+                items = items[start+len(startingtag):]
+                end = items.index(endtag)
+                result = items[:end]
+        counter=counter+1
+        print(result)
+    print(counter)
         
     
 def main():
