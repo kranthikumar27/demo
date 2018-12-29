d8f0a2bcd0428cf64f13f7cbed0d17eeb14c4f8c
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-12-29 11:11:22 +0530

    "cspp1_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 4/10 "

--- cspp1-practice/remideals/CSPP-1Week1Exam/webpage.py
+++ cspp1-practice/remideals/CSPP-1Week1Exam/webpage.py
@@ -1,44 +1,43 @@
 def getBackground(color):
-    line=color.split("background-color")
-    # print(lines)
-    startingtag=":\""
-    endtag=";\""
-    bgcolor=[]
-    for i in line:
-        bgcolor.append(i)
+    # lines=color.split("back")
+    # # print(lines)
+    # startingtag="ground-color:\""
+    # endtag=";\""
+    # bgcolor=[]
+    # for i in lines:
+    #     bgcolor.append(i)
+    #     # print(bgcolor)
+    #     for j in bgcolor:
+    #         if startingtag in j:
+    #             start = j.index(startingtag)
+    #             items = j[start+len(startingtag):]
+    #             end = j.index(endtag)
+    #             results = j[:end]
+    #             print(results)
         # print(bgcolor)
-        print("kranthi")
-        for j in bgcolor:
-            if startingtag in j:
-                start = j.index(startingtag)
-                items = j[start+len(startingtag):]
-                end = j.index(endtag)
-                # print("kranthi")
-                results = j[:end]
-                print(results)
-        # print(bgcolor)
-
+        pass
 
 def getImageUrl(urls):
-    # lines = urls.split("<img src")
-    # # print(lines)
-    # startingtag="=\""
-    # endtag = "\""
-    # image=[]
-    # counter=0
-    # for i in lines:
-    #     image.append(i)
-    #     # print(image)
-    #     for items in image:
-    #         if startingtag in items:
-    #             start = items.index(startingtag)
-    #             items = items[start+len(startingtag):]
-    #             end = items.index(endtag)
-    #             result = items[:end]
-    #             counter=counter+1
-    #             print(result)
-    # print(counter)
-    pass
+    lines = urls.split("<img src")
+    # print(lines)
+    startingtag="=\""
+    endtag = "\""
+    image=[]
+    counter=0
+    for i in lines:
+        image.append(i)
+        # print(image)
+        for items in image:
+            if startingtag in items:
+                start = items.index(startingtag)
+                items = items[start+len(startingtag):]
+                end = items.index(endtag)
+                result = items[:end]
+                unique=set(result)
+                counter=counter+1
+                print(unique)
+    print(counter)
+
 def main():
     data = open("webpage5.html", errors="ignore").read()
     # print(data)
