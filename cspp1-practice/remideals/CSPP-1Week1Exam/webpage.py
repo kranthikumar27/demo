6d9df921f3114d0ecaf6cfa2679762956a41ccaa
kranthi27 <kranthikumarpinku@msitprogram.net>
2019-01-02 16:53:32 +0530

    "cspp1_m10_a1_1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- cspp1-practice/remideals/CSPP-1Week1Exam/webpage.py
+++ cspp1-practice/remideals/CSPP-1Week1Exam/webpage.py
@@ -1,57 +1,81 @@
-def background(color):
-    bgcolor=[]
-    linewithcolor=color.split(";")
-    initialtag="background-color:\""
-    endtagg=";\""
-    answer=[]
-    count=0
-    for i in linewithcolor:
-        if "background-color:" in i:
-            start = i.index(initialtag)
-            i = i[start+len(initialtag):]
-            end = i.index(endtagg)
-            answer.append(i[end:])
-            print(answer)            
-        count=count+1
-        print(answer)
-    print(count)
+def background(data):
+	bgcolor=[]
+	linewithcolor=data.split(";")
+	# print(linewithcolor)
+	startingtag="background-color"
+	endtag=";"
+	result=[]
+	tag=":"
+	count=0
+	for items in linewithcolor:
+		if "background-color" in items:
+			start = items.index(startingtag)
+			items = items[start+len(startingtag):]
+			end = items.index(tag)
+			result = items[end:]
+				# count =count+1
+				# result=set(result)+
+				# 
+			print(result)
+	# print(count)
+	result=set(result)
+	# print("............sort.........")
+	# print(result)
+	count=0
+	final=[]
+	for i in result:
+		print(i)
+		if "!" in i:
+			i = i[1:].replace(" ","")
+			final.append(i)
+			count = count+1
+	for j in final:
+		if "}" in j:
+			index=j.index("}")
+			j=j[:index]
+			final.append(j)
+	bgcolor = sorted(final)
+	print(bgcolor)
+	#     count=count+1
+	#     print(result)
+	# print(count)
 
 def image(urls):
-    lines = urls.split("<img")
-    # print(lines)
-    lines = lines[1:]
-    startingtag="src=\""
-    endtag = "\""
-    image=[]
-    counter=0
-    for i in lines:
-        image.append(i)
-        # print(image)
-        for items in image:
-            if startingtag in items:
-                start = items.index(startingtag)
-                items = items[start+len(startingtag):]
-                end = items.index(endtag)
-                result = items[:end]
-        counter=counter+1
-        print(result)
-    print(counter)
-        
+	lines = urls.split("<img")
+	# print(lines)
+	lines = lines[1:]
+	startingtag="src=\""
+	endtag = "\""
+	image=[]
+	counter=0
+	for i in lines:
+		image.append(i)
+		# print(image)
+		for items in image:
+			if startingtag in items:
+				start = items.index(startingtag)
+				items = items[start+len(startingtag):]
+				end = items.index(endtag)
+				result = items[:end]
+		counter=counter+1
+		print(result)
+	print(counter)
+		
 def list1(data):
-    pass
+	pass
 def main():
-    data = open("webpage5.html", errors="ignore").read()
-    # print(data)
-    inputdata = input()
-    if inputdata == "image":
-        image(data)
-    elif inputdata == "background":
-        background(data)
-    elif inputdata == "list":
-        list1(data)
-    # image(data)
-    
+	data = open("webpage5.html", errors="ignore").read()
+	# print(data)
+	inputdata = input()
+	if inputdata == "image":
+		image(data)
+	elif inputdata == "background":
+		background(data)
+	elif inputdata == "list":
+		list1(data)
+	# image(data)
+	
 
 
 if __name__ == '__main__':
-    main()
+	main()
