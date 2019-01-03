7c9795714fb3569e905da5ed89430b628129ce44
kranthi27 <kranthikumarpinku@msitprogram.net>
2019-01-03 17:48:46 +0530

    "cspp2_m10_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ cspp1-practice/remideals/CSPP2 Remedial Exam/reservation.py
@@ -0,0 +1,92 @@
+class reservation:
+    def __init__(self, cname, roomnumber=50):
+        self.cname=cname
+        self.roomnumber=roomnumber
+    # def __init__(self, cname):
+    #   self.cname = cname
+    def setroomnumber(self,newroomnumber):
+        self.roomnumber=newroomnumber
+    def setcname(self,newcname):
+        self.cname=newcname
+    def getroomnumber(self):
+        return self.roomnumber
+    def getcname(self):
+        return self.cname
+
+class hotel:
+    def __init__(self):
+        self.rooms=[None, None, None, None, None]
+        self.size=5
+        self.count=0
+    
+    def reserveRoom(self, cname):
+        if self.size==self.count:
+            print("All Rooms are reserved")
+            return
+        for i in range(self.size):
+            if self.rooms[i]==None:
+                obj=reservation(cname)
+                obj.setroomnumber(i+1)
+                self.rooms[i] = obj
+                self.count += 1
+                return i+1
+        return -1
+    def reservingRoom(self, cname, roomnumber):
+        if self.size == self.count:
+            print("All Rooms are reserved")
+            return False
+        elif (self.rooms[roomnumber-1] == None):
+            obj1 = reservation(cname,roomnumber)
+            self.rooms[roomnumber-1] = obj1
+            self.count += 1
+            return True
+        elif (self.rooms[roomnumber] != None):
+            print("Room is already reserved")
+        return False
+
+    def printReservations(self):
+        for i in range(self.count):
+            print(self.rooms[i].getcname()," ", self.rooms[i].getroomnumber())
+    
+    def cancelReservations(self, cname):
+        pass
+class Solution1:
+    hotel=hotel()
+    input_data=int(input())
+    c=0
+    while c<input_data:
+        tokens = str(input())
+        tokens=tokens.split(" ")
+        if tokens[0] == "reserve":
+            cname = tokens[1]
+            roomnumber = -1
+            if cname!= None:
+                roomnumber = hotel.reserveRoom(cname)
+            elif roomnumber != -1:
+                print(cname," ",roomnumber)
+
+        if tokens[0] == "reserveN":
+            cname = tokens[1]
+            if cname == None:
+                roomnumber = -1
+            else :
+                roomnumber = int(tokens[2])
+            if(hotel.reservingRoom(cname,roomnumber)):
+                print(cname," ",roomnumber)
+
+        if tokens[0] == "print":
+            hotel.printReservations()
+        
+
+        if tokens[0] == "cancel":
+            cname = tokens[1]
+            if cname!=null:
+                hotel.cancelReservations(cname)
+                print(cname," now has no reservations.")
+            print("No input")
+        c=c+1
+
+def main():
+    if __name__ == '__main__':
+        main()
+
